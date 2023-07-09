5605ae905868098d3eab164a19646a1f51574b52
RouxAntoine <antoinroux@hotmail.fr>
2023-07-09 11:52:48 +0200

    fix: resolve bug with path prefix on server url
    Close Issue 1370

--- dist/index.js
+++ dist/index.js
@@ -2449,7 +2449,7 @@ function getFetchUrl(settings) {
         return `${user}@${serviceUrl.hostname}:${encodedOwner}/${encodedName}.git`;
     }
     // "origin" is SCHEME://HOSTNAME[:PORT]
-    return `${serviceUrl.origin}/${encodedOwner}/${encodedName}`;
+    return `${serviceUrl.origin}${serviceUrl.pathname}/${encodedOwner}/${encodedName}`;
 }
 exports.getFetchUrl = getFetchUrl;
 function getServerUrl(url) {
